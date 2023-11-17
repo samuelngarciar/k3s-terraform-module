38486938e1751c0528d241b940ccb0a91a13df3f
Samuel Garcia <eb_sgarciar@bancoripley.com>
2023-11-17 07:33:57 -0400

    del outoputs content

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,1 @@
-output "execution_time" {
-  value = "${null_resource.k3s_node.triggers["timestamp"]}"
-  description = "The time at which the local-exec provisioner was executed."
-}
 
-output "resource_id" {
-  value = "${null_resource.k3s_node.id}"
-  description = "The ID of the null resource."
-}
