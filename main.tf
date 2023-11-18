7ff9b5217d82f97d013706440d24d06ed566ed78
Samuel Garcia <eb_sgarciar@bancoripley.com>
2023-11-18 11:45:00 -0400

    add variable: k3stoken

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "null" {}
 resource "null_resource" "k3s_node" {
 provisioner "local-exec" {
   command = <<-EOT
-    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${var.k3srelease}" INSTALL_K3S_EXEC='--node-name=k3s-node' sh -
+    curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${var.k3srelease}" INSTALL_K3S_EXEC='--node-name=k3s-node' sh -s - --token ${var.k3stoken}
   EOT
 }
 }
--- variables.tf
+++ variables.tf
@@ -4,3 +4,10 @@ variable "k3srelease" {
   type        = string
   default     = "v1.27.4+k3s1"
 }
+
+
+variable "k3stoken" {
+  description = "token of the server"
+  type        = number
+  default     = 123567891
+}
